14d1f7447cccf52db96c6396e533ee2e0608c7bc
IWillRekU3206 <37181282+iwillreku3206@users.noreply.github.com>
2023-08-01 18:50:20 +0800

    minimal exmaple

--- examples/basic/src/app/page.tsx
+++ examples/basic/src/app/page.tsx
@@ -1,13 +1,9 @@
-import getConfig from 'next/config'
-import Image from 'next/image'
-import { translations } from './translations'
+import { translations } from '../translations'
 import { getUserLanguageFromRequest } from 'nextjs-translations/dist/server/getUserLanguage'
-import getT from 'nextjs-translations/dist/server/t'
+import { getT } from 'nextjs-translations'
 
 export default function Home() {
-  const cfg = getConfig()
   const t = getT(translations)
-  console.log()
 
   return (
     <main className="overflow-auto">
