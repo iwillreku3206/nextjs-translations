566570d34f653bdb98ac87c3215f3d06f824f8e9
IWillRekU3206 <37181282+iwillreku3206@users.noreply.github.com>
2023-08-07 19:43:28 +0800

    change root to use commonjs

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,10 @@
 // prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
 
 /** @type {import("prettier").Options} */
-const config = {
+module.exports = {
 	trailingComma: "es5",
 	tabWidth: 2,
 	useTabs: true,
 	semi: false,
 	singleQuote: true,
 };
-
-export default config;
